879b0f687cced97c603771ae08a8715aa4289304
matthiasvanmec <103990322+matthiasvanmec@users.noreply.github.com>
2026-08-26 13:46:02 +0200

    simply executed the code, files taht changed are the main file to specify which sim to run, or results of the sim

--- testcases/nozzle_steady_two_phase/main.py
+++ testcases/nozzle_steady_two_phase/main.py
@@ -5,7 +5,7 @@ t = Timer()
 
 t.start()
 
-# configFile = "inputs/config_files/lettieri/L1_smooth.ini"
+configFile = "inputs/config_files/lettieri/L1_smooth.ini"
 # configFile = "inputs/config_files/lettieri/L1_friction.ini"
 # configFile = "inputs/config_files/lettieri/L5_smooth.ini"
 # configFile = "inputs/config_files/lettieri/L5_friction.ini"
@@ -23,15 +23,17 @@ t.start()
 # configFile = "inputs/config_files/CM-15.3/roe_arabi/single_phase_gas.ini"
 # configFile = "inputs/config_files/CM-15.3/roe_arabi/two_phase.ini"
 # configFile = "inputs/config_files/CM-15.3/roe_vinokur/phase_transition.ini"
-configFile = "inputs/config_files/CM-15.3/roe_vinokur/single_phase_gas.ini"
+# configFile = "inputs/config_files/CM-15.3/roe_vinokur/single_phase_gas.ini"
 # configFile = "inputs/config_files/CM-15.3/roe_vinokur/two_phase.ini"
 
 # configFile = "inputs/config_files/CM-10.2/test_conf.ini"
-
+from benchmarking_tools.timing import Timer
+t = Timer()
+t.start()
 config = Config(configFile)
 driver = Driver(config=config)
 driver.solve()
- 
+t.stop()
 # config = Config(configFilePath = configFile)
 # driver = Driver(config = config) 
 # driver.solve()
